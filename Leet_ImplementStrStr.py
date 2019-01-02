079fd2ccb6f1a6678a257418f1dd21ab50742b1a
Parul M <silver.1.icicle@gmail.com>
2019-01-02 11:13:58 -0600

    Leet_Implement C's StrStr()

---
+++ Leet_ImplementStrStr.py
@@ -0,0 +1,31 @@
+'''
+Problem Statement: Implement strStr().
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+Input: haystack = "hello", needle = "ll"
+Output: 2
+
+Example 2:
+Input: haystack = "aaaaa", needle = "bba"
+Output: -1
+'''
+
+class Solution:
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        try:
+            return haystack.index(needle)    # Returns the index value if substring is found
+        except ValueError:                   # Throws a <ValueError> if the substring is not found 
+            return -1
+
+if __name__ == '__main__':
+    obj = Solution()
+    h = "Apple"
+    n = "pp"
+    print(obj.strStr(h,n))
+        
